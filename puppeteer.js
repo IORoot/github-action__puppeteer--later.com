37c0f9386280e462cd1edead6a4bb40e65c81a12
Andy Pearson <andy@londonparkour.com>
2021-08-18 09:30:47 +0100

    screenshot

--- puppeteer.js
+++ puppeteer.js
@@ -69,17 +69,25 @@ var runner = (function () {
                 return;
             } 
             
+            console.log('goto page');
             await page.goto('https://app.later.com/user/login', { waitUntil: "networkidle2" });
 
+            console.log('screenshot');
             await page.screenshot({path: './screenshot.png', fullPage: true});
             
+            console.log('wait for selector');
             await page.waitForSelector('#ember6');
+
+            console.log('click #ember6');
             await page.click('#ember6');
             
+            console.log('waitForNavigation');
             await page.waitForNavigation();
             
+            console.log('type into #ember6');
             await page.type('#ember6', username);
             
+            console.log('type into #ember7');
             await page.type('#ember7', password);
             
             await page.waitForSelector('.ember-application > .ember-view > .tOB--container > .tSU--container:nth-child(2)');
