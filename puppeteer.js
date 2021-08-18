adabd563a49ec3ae89bae930a1154fd0ab920f65
Andy Pearson <andy@londonparkour.com>
2021-08-18 17:06:11 +0100

    click upload

--- puppeteer.js
+++ puppeteer.js
@@ -84,37 +84,48 @@ var runner = (function () {
 
 
             try {
-            let agent = await page.browser().userAgent();
-            console.log('Current UserAgent:' + agent);
-            await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36');
+                console.log('Changing user-agent');
+                await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36');
             } catch {
                 console.log('Unable to change UserAgent : ' + err);
                 return;
             }
 
+            try {
+                console.log('Navigating to page');
+                await page.goto('https://app.later.com/user/login', { waitUntil: "networkidle2" });
+                
+                console.log('wait for selector');
+                await page.waitForSelector('#ember6');
 
-            console.log('goto page');
-            await page.goto('https://app.later.com/user/login', { waitUntil: "networkidle2" });
-            
-            console.log('wait for selector');
-            await page.waitForSelector('#ember6');
+                console.log('click #ember6');
+                await page.click('#ember6');
+                
+                console.log('type into #ember6 (username)');
+                await page.type('#ember6', username);
+                
+                console.log('type into #ember7 (password)');
+                await page.type('#ember7', password);
 
-            console.log('click #ember6');
-            await page.click('#ember6');
-            
-            console.log('type into #ember6');
-            await page.type('#ember6', username);
-            
-            console.log('type into #ember7');
-            await page.type('#ember7', password);
+                console.log('screenshot1');
+                await page.screenshot({path: './screenshot1.png', fullPage: true});
 
-            console.log('screenshot1');
-            await page.screenshot({path: './screenshot1.png', fullPage: true});
+                console.log('click login button');
+                await page.click('.qa--login__btn');
+                
+                await page.waitForTimeout(10000);
+            } catch {
+                console.log('Unable to login : ' + err);
+                return;
+            }
 
-            console.log('click button');
-            await page.click('.qa--login__btn');
-            
-            await page.waitForTimeout(10000);
+            try {
+                console.log('click upload button');
+                await page.click('.cUP--upload__text.is--base');
+            } catch {
+                console.log('Unable to upload video : ' + err);
+                return;
+            }
 
             console.log('screenshot2');
             await page.screenshot({path: './screenshot2.png', fullPage: true});
