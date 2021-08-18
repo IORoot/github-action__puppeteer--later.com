fcabd1ce808f77d5f9fdbab9d72db23a2ab7240b
Andy Pearson <andy@londonparkour.com>
2021-08-18 09:37:35 +0100

    screenshot

--- puppeteer.js
+++ puppeteer.js
@@ -75,28 +75,28 @@ var runner = (function () {
             console.log('screenshot');
             await page.screenshot({path: './screenshot.png', fullPage: true});
             
-            console.log('wait for selector');
-            await page.waitForSelector('#ember6');
+            // console.log('wait for selector');
+            // await page.waitForSelector('#ember6');
 
-            console.log('click #ember6');
-            await page.click('#ember6');
+            // console.log('click #ember6');
+            // await page.click('#ember6');
             
-            console.log('waitForNavigation');
-            await page.waitForNavigation();
+            // console.log('waitForNavigation');
+            // await page.waitForNavigation();
             
-            console.log('type into #ember6');
-            await page.type('#ember6', username);
+            // console.log('type into #ember6');
+            // await page.type('#ember6', username);
             
-            console.log('type into #ember7');
-            await page.type('#ember7', password);
+            // console.log('type into #ember7');
+            // await page.type('#ember7', password);
             
-            await page.waitForSelector('.ember-application > .ember-view > .tOB--container > .tSU--container:nth-child(2)');
-            await page.click('.ember-application > .ember-view > .tOB--container > .tSU--container:nth-child(2)');
+            // await page.waitForSelector('.ember-application > .ember-view > .tOB--container > .tSU--container:nth-child(2)');
+            // await page.click('.ember-application > .ember-view > .tOB--container > .tSU--container:nth-child(2)');
             
-            await page.waitForSelector('.tSU--container__row > .tSU--card > .u--p__t__lg > .o--formSubmit > .qa--login__btn');
-            await page.click('.tSU--container__row > .tSU--card > .u--p__t__lg > .o--formSubmit > .qa--login__btn');
+            // await page.waitForSelector('.tSU--container__row > .tSU--card > .u--p__t__lg > .o--formSubmit > .qa--login__btn');
+            // await page.click('.tSU--container__row > .tSU--card > .u--p__t__lg > .o--formSubmit > .qa--login__btn');
             
-            await page.screenshot({ path: 'screenshot_1.png', fullPage: true });
+            // await page.screenshot({ path: 'screenshot_1.png', fullPage: true });
             
             await browser.close();
         
