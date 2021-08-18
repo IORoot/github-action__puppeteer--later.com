c87553783a1d5457fc99c501ba8d997268fe0ada
Andy Pearson <andy@londonparkour.com>
2021-08-18 13:56:15 +0100

    login and screenshot

--- puppeteer.js
+++ puppeteer.js
@@ -73,42 +73,42 @@ var runner = (function () {
             console.log('browser version:' + version);
 
 
+            try {
             let agent = await page.browser().userAgent();
-            console.log('before user-agent:' + agent);
-            // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+            console.log('Current UserAgent:' + agent);
             await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36');
-            let agenttwo = await page.browser().userAgent();
-            console.log('after user-agent:' + agenttwo);
+            } catch {
+                console.log('Unable to change UserAgent : ' + err);
+                return;
+            }
 
 
             console.log('goto page');
             await page.goto('https://app.later.com/user/login', { waitUntil: "networkidle2" });
+            
+            console.log('wait for selector');
+            await page.waitForSelector('#ember6');
 
+            console.log('click #ember6');
+            await page.click('#ember6');
+            
+            console.log('waitForNavigation');
+            await page.waitForNavigation();
+            
+            console.log('type into #ember6');
+            await page.type('#ember6', username);
+            
+            console.log('type into #ember7');
+            await page.type('#ember7', password);
+            
+            await page.waitForSelector('.ember-application > .ember-view > .tOB--container > .tSU--container:nth-child(2)');
+            await page.click('.ember-application > .ember-view > .tOB--container > .tSU--container:nth-child(2)');
+            
+            await page.waitForSelector('.tSU--container__row > .tSU--card > .u--p__t__lg > .o--formSubmit > .qa--login__btn');
+            await page.click('.tSU--container__row > .tSU--card > .u--p__t__lg > .o--formSubmit > .qa--login__btn');
+            
             console.log('screenshot');
             await page.screenshot({path: './screenshot.png', fullPage: true});
-            
-            // console.log('wait for selector');
-            // await page.waitForSelector('#ember6');
-
-            // console.log('click #ember6');
-            // await page.click('#ember6');
-            
-            // console.log('waitForNavigation');
-            // await page.waitForNavigation();
-            
-            // console.log('type into #ember6');
-            // await page.type('#ember6', username);
-            
-            // console.log('type into #ember7');
-            // await page.type('#ember7', password);
-            
-            // await page.waitForSelector('.ember-application > .ember-view > .tOB--container > .tSU--container:nth-child(2)');
-            // await page.click('.ember-application > .ember-view > .tOB--container > .tSU--container:nth-child(2)');
-            
-            // await page.waitForSelector('.tSU--container__row > .tSU--card > .u--p__t__lg > .o--formSubmit > .qa--login__btn');
-            // await page.click('.tSU--container__row > .tSU--card > .u--p__t__lg > .o--formSubmit > .qa--login__btn');
-            
-            // await page.screenshot({ path: 'screenshot_1.png', fullPage: true });
             
             await browser.close();
         
