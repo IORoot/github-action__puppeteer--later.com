0ac7ac2cadd10967604595299321217d1472351a
Andy Pearson <andy@londonparkour.com>
2021-09-20 14:47:59 +0100

    alert container

--- puppeteer.js
+++ puppeteer.js
@@ -4,10 +4,10 @@ var runner = (function () {
     const puppeteer = require('puppeteer-core');
 
     let puppeteer_settings = { 
-        headless: false, 
+        headless: true, 
         devtools: false,
-        // executablePath: "/usr/bin/google-chrome-stable",
-        executablePath: "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome",
+        executablePath: "/usr/bin/google-chrome-stable",
+        // executablePath: "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome",
         args: ['--no-sandbox']
     }
 
